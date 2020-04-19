b11bf76131a1b0263ec7abbae958a243582ebd13
Kush <kushal@campion.ie>
2020-04-19 19:17:53 +0100

    communication between view and controller in kotlin way

--- app/src/main/java/com/kush/learningkotlin/MainActivity.kt
+++ app/src/main/java/com/kush/learningkotlin/MainActivity.kt
@@ -1,18 +1,114 @@
 package com.kush.learningkotlin
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
 import android.view.View
+import android.widget.CheckBox
+import android.widget.Toast
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
+import org.w3c.dom.Text
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    val marsGravity: Float = 0.38f
+    val jupiterGravity: Float = 2.4f
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var enterWeight = weightET.text
+
         cilckMeBtn.setOnClickListener {
-            displayTV.text = "Thanks For Clicking.."
+            var weightOnOtherPlanet = calculateWeighOnMars(enterWeight.toString().toDouble(),
+                                   marsCheckBox.isChecked, jupiterCheckBox.isChecked)
+            if (enterWeight.isEmpty()) {
+                displayTV.text = "Please enter your weight for calculation"
+            } else {
+                displayTV.text = weightOnOtherPlanet
+            }
+        }
+
+        marsCheckBox.setOnClickListener(this)
+        jupiterCheckBox.setOnClickListener(this)
+
+        if (TextUtils.isEmpty(weightET.text.toString())) {
+            marsCheckBox.visibility = View.INVISIBLE
+            jupiterCheckBox.visibility = View.INVISIBLE
+        }
+
+        weightET.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                if (TextUtils.isEmpty(weightET.text.toString())) {
+                    marsCheckBox.visibility = View.INVISIBLE
+                    jupiterCheckBox.visibility = View.INVISIBLE
+                }
+            }
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) { }
+
+            override fun onTextChanged(p0: CharSequence?, start: Int, before: Int, count: Int) {
+                marsCheckBox.visibility = View.VISIBLE
+                jupiterCheckBox.visibility = View.VISIBLE
+            }
+        })
+
+    }
+
+    fun Double.format(digit: Int) =
+        java.lang.String.format("%.${digit}f", this)
+
+    /*fun planetClicked(view: View) {
+        view as CheckBox
+        var whichPlanetChecked: Boolean = view.isChecked
+        when (view.id) {
+            R.id.marsCheckBox -> {
+                if (whichPlanetChecked) {
+                    Toast.makeText(this, "Mars Checked", Toast.LENGTH_LONG).show()
+                }
+            }
+            R.id.jupiterCheckBox -> {
+                if (whichPlanetChecked) {
+                    Toast.makeText(this, "Jupiter Checked", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+
+    }*/
+
+    private fun calculateWeighOnMars(
+        earthWeight: Double,
+        marsChecked: Boolean,
+        jupiterChecked: Boolean
+    ): String {
+        if (marsChecked) {
+            return "Your weight on Mars is: " + (earthWeight * marsGravity).format(2)
+        } else {
+            return "Your weight on Jupiter is: " + (earthWeight * jupiterGravity).format(2)
+        }
+    }
+
+    override fun onClick(v: View?) {
+        v as CheckBox
+        var isChecked: Boolean = v.isChecked
+
+        when (v.id) {
+            R.id.marsCheckBox -> {
+                if(isChecked && !TextUtils.isEmpty(weightET.text.toString())) {
+                    Toast.makeText(this, "Mars Clicked", Toast.LENGTH_LONG).show()
+                    jupiterCheckBox.isChecked = false
+                }
+            }
+            R.id.jupiterCheckBox -> {
+                if (isChecked && !TextUtils.isEmpty(weightET.text.toString())) {
+                    Toast.makeText(this, "Jupiter Clicked", Toast.LENGTH_LONG).show()
+                    marsCheckBox.isChecked = false
+                }
+            }
         }
     }
 }
